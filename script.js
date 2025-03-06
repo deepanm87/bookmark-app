ffe1e17c70903087f4e479b1356e08e42cfac950
deepanm87 <deepan.murugan29@gmail.com>
2025-03-06 15:36:50 -0500

    added code to add urls to dom

--- script.js
+++ script.js
@@ -37,6 +37,30 @@ function validate(nameValue, urlValue) {
     return true
 }
 
+function buildBookmarks() {
+    bookmarks.forEach((bookmark) => {
+        const { name, url} = bookmark
+        const item = document.createElement('div')
+        item.classList.add('item')
+        const closeIcon = document.createElement('i')
+        closeIcon.classList.add('fas', 'fa-times')
+        closeIcon.setAttribute('title', 'Delete Bookmark')
+        closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`)
+        const linkInfo = document.createElement('div')
+        linkInfo.classList.add('name')
+        const favicon = document.createElement('img')
+        favicon.setAttribute('src', `https://www.google.com/s2/u/0/favicons?domain=${url}`)
+        favicon.setAttribute('alt', 'Favicon')
+        const link = document.createElement('a')
+        link.setAttribute('href', `${url}`)
+        link.setAttribute('target', '_blank')
+        link.textContent = name
+        linkInfo.append(favicon, link)
+        item.append(closeIcon, linkInfo)
+        bookmarksContainer.appendChild(item)
+    })
+}
+
 function fetchBookmarks() {
     if (localStorage.getItem('bookmarks')) {
         bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
@@ -49,6 +73,7 @@ function fetchBookmarks() {
         ]
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
     }
+    buildBookmarks()
 
 }
 
