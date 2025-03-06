b64f336be8bdc42caeb944cdd3c670d9f67b5526
deepanm87 <deepan.murugan29@gmail.com>
2025-03-06 14:35:00 -0500

    added local storage functionality

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById('website-name')
 const websiteUrlEl = document.getElementById('website-url')
 const bookmarksContainer = document.getElementById('bookmarks-container')
 
+let bookmarks = []
+
 function showModal() {
     modal.classList.add('show-modal')
     websiteNameEl.focus()
@@ -35,6 +37,21 @@ function validate(nameValue, urlValue) {
     return true
 }
 
+function fetchBookmarks() {
+    if (localStorage.getItem('bookmarks')) {
+        bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
+    } else {
+        bookmarks = [
+            {
+                name: 'Google',
+                url: 'https://google.com'
+            }
+        ]
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+    }
+
+}
+
 function storeBookmark(e) {
     e.preventDefault()
     const nameValue = websiteNameEl.value
@@ -45,7 +62,19 @@ function storeBookmark(e) {
     if (!validate(nameValue, urlValue)) {
         return false
     }
+    const bookmark = {
+        name: nameValue,
+        url: urlValue
+    }
+    bookmarks.push(bookmark)
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+    fetchBookmarks()
+    bookmarkForm.reset()
+    websiteNameEl.focus()
+
 }
 
 bookmarkForm.addEventListener('submit', storeBookmark)
+
+fetchBookmarks()
 
